359d7f908f7942df0224e6e422ad7b7b9f39337b
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2016-09-03 05:03:49 +0800

    added db

--- main.go
+++ main.go
@@ -19,10 +19,21 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/garyburd/redigo/redis"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 var bot *linebot.Client
+
+func connectDB(url string) redis.Conn {
+	c, redisErr := redis.DialURL(url)
+	if redisErr != nil {
+		log.Println("Connect to redis error", redisErr)
+		return nil
+	}
+
+	return c
+}
 
 func main() {
 	strID := os.Getenv("ChannelID")
@@ -63,17 +74,45 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			switch text.Text {
 			case "加我":
 				if user.Count == 1 {
-					_, err = bot.SendText([]string{content.From}, user.Contacts[0].DisplayName+" 您好，已將您加入 ^_^ "+content.From)
-					if err != nil {
-						log.Println(err)
+					c := connectDB(os.Getenv("REDISTOGO_URL"))
+					n, appendErr := c.Do("SET", user.Contacts[0].MID, content.From)
+					if appendErr != nil {
+						log.Println("SET to redis error", appendErr, n)
+					} else {
+						_, err = bot.SendText([]string{content.From}, user.Contacts[0].DisplayName+" 您好，已將您加入 ^_^ "+content.From)
+						if err != nil {
+							log.Println(err)
+						}
 					}
+					defer c.Close()
 				}
 			case "退出":
 				if user.Count == 1 {
-					_, err = bot.SendText([]string{content.From}, user.Contacts[0].DisplayName+" 掰掰 QQ")
-					if err != nil {
-						log.Println(err)
+					c := connectDB(os.Getenv("REDISTOGO_URL"))
+					n, setErr := c.Do("DEL", user.Contacts[0].MID)
+					if setErr != nil {
+						log.Println("DEL to redis error", setErr, n)
+					} else {
+						_, err = bot.SendText([]string{content.From}, user.Contacts[0].DisplayName+" 掰掰 QQ")
+						if err != nil {
+							log.Println(err)
+						}
 					}
+					defer c.Close()
+				}
+			case "關於我":
+				if user.Count == 1 {
+					c := connectDB(os.Getenv("REDISTOGO_URL"))
+					id, getErr := redis.String(c.Do("GET", user.Contacts[0].MID))
+					if getErr != nil {
+						fmt.Println("redis get failed:", getErr)
+					} else {
+						_, err = bot.SendText([]string{content.From}, id)
+						if err != nil {
+							log.Println(err)
+						}
+					}
+					defer c.Close()
 				}
 			}
 		}
