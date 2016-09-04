c6639682d99c968c9b9dcf4a3666e8d1bcb78b9a
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2016-09-04 23:30:07 +0800

    fixed

--- worker/main.go
+++ worker/main.go
@@ -30,7 +30,7 @@ func main() {
 	for {
 		c := db.Connect(os.Getenv("REDISTOGO_URL"))
 
-		targets0 := []string{"新竹市"}
+		targets0 := []string{"基隆市"}
 		msgs0, token0 := rain.GetRainingInfo(targets0)
 
 		status0, getErr := redis.Int(c.Do("SISMEMBER", "token0", token0))
@@ -71,7 +71,7 @@ func main() {
 			log.Println("SADD to redis error", addErr, n0)
 		}
 
-		targets1 := []string{"新竹市", "新竹縣", "台中市", "高雄市", "台北市"}
+		targets1 := []string{"基隆市", "台北市"}
 		msgs1, token1 := rain.GetWarningInfo(targets1)
 
 		status1, getErr := redis.Int(c.Do("SISMEMBER", "token1", token1))
@@ -95,7 +95,6 @@ func main() {
 					local = local.In(location)
 				}
 				log.Println(users1)
-				log.Println(msgs0)
 				log.Println(msgs1)
 				for _, contentTo := range users1 {
 					for _, msg := range msgs1 {
