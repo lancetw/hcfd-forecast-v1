29dffc9e3a9cb9074f307f413ebdaee14cc20cdf
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2016-09-07 10:47:31 +0800

    fixed format

--- rain/rain.go
+++ rain/rain.go
@@ -135,38 +135,38 @@ func GetRainingInfo(targets []string, noLevel bool) ([]string, string) {
 							switch element.Name {
 							case "MIN_10":
 								if noLevel {
-									if element.Value < 0 {
-										msg = msg + fmt.Sprintf("%s：%s", "10分鐘雨量", "-")
+									if element.Value <= 0 {
+										msg = msg + fmt.Sprintf("%s：%s", "*10分鐘雨量*", "-")
 									} else {
-										msg = msg + fmt.Sprintf("%s：%.2f", "10分鐘雨量", element.Value)
+										msg = msg + fmt.Sprintf("%s：%.1f", "*10分鐘雨量*", element.Value)
 									}
 								} else {
-									if element.Value < 0 {
-										log.Printf("%s：%s", "10分鐘雨量", "-")
+									if element.Value <= 0 {
+										log.Printf("%s：%s", "*10分鐘雨量*", "-")
 									} else {
-										log.Printf("%s：%.2f", "10分鐘雨量", element.Value)
+										log.Printf("%s：%.1f", "*10分鐘雨量*", element.Value)
 										if element.Value >= rainLevel["10minutes"] {
-											msg = msg + fmt.Sprintf("【%s】*豪大雨警報*\n%s：%.2f \n", location.Name, "10分鐘雨量", element.Value)
+											msg = msg + fmt.Sprintf("【%s】*豪大雨警報*\n%s：%.1f \n", location.Name, "10分鐘雨量", element.Value)
 										}
 									}
 								}
 
 							case "RAIN":
 								if noLevel {
-									if element.Value < 0 {
+									if element.Value <= 0 {
 										msg = msg + fmt.Sprintf("【%s】\n%s：%s\n", location.Name, "時雨量", "-")
 									} else {
-										msg = msg + fmt.Sprintf("【%s】\n%s：%.2f\n", location.Name, "時雨量", element.Value)
+										msg = msg + fmt.Sprintf("【%s】\n%s：%.1f\n", location.Name, "時雨量", element.Value)
 									}
 								} else {
-									if element.Value < 0 {
+									if element.Value <= 0 {
 										log.Printf("[%s]", location.Name)
 										log.Printf("%s：%s", "時雨量", "-")
 									} else {
 										log.Printf("[%s]", location.Name)
-										log.Printf("%s：%.2f", "時雨量", element.Value)
+										log.Printf("%s：%.1f", "時雨量", element.Value)
 										if element.Value >= rainLevel["1hour"] {
-											msg = msg + fmt.Sprintf("【%s】*豪大雨警報*\n%s：%.2f \n", location.Name, "時雨量", element.Value)
+											msg = msg + fmt.Sprintf("【%s】*豪大雨警報*\n%s：%.1f \n", location.Name, "時雨量", element.Value)
 										}
 									}
 								}
