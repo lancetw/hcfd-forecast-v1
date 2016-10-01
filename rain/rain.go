84f05f9d2111a5543eb83061a8fab0b5274a3359
Hsin-lin Cheng <lancetw@gmail.com>
2016-10-01 18:37:23 +0800

    Update rain.go

--- rain/rain.go
+++ rain/rain.go
@@ -213,9 +213,7 @@ func GetWarningInfo(targets []string) ([]string, string) {
 	var hazardmsgs = ""
 
 	for i, location := range v.Location {
-		if i == 0 {
-			token = location.Hazards.ValidTime.StartTime.Format("20060102150405") + " " + location.Hazards.ValidTime.EndTime.Format("20060102150405")
-		}
+		token = token + location.Hazards.ValidTime.StartTime.Format("20060102150405") + " " + location.Hazards.ValidTime.EndTime.Format("20060102150405")
 		if location.Hazards.Info.Phenomena != "" && location.Hazards.ValidTime.EndTime.After(local) {
 			if targets != nil {
 				for _, name := range targets {
